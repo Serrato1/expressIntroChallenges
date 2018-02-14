54cd037888cc836b560cc7ff326aaadbeacdd6c7
Noel Serrato <Serrato@LGMBP.local>
2018-02-14 14:57:22 -0700

    Finishedd

--- part2/server.js
+++ part2/server.js
@@ -1,15 +1,69 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
-});
+//Part two
+//Challenge 1 + Modified for Challenge 4 Unique Ids
+app.get('/create/:name/:age',(req,res)=>{
+  let name = req.params.name;
+  let age = req.params.age;
+  let origJson = fs.readFileSync('./storage.json','utf8', (err)=>{
+    if(err){console.log(err);}
+  });
+  origJson = JSON.parse(origJson);
+  let users = origJson.users;
+  if(users.length !== 0 ){
+    var uniq_id = users[users.length-1]['id'] + 1;
+  }else{
+    var uniq_id = 1;
+  }
+  let usr = {
+    'id'   : uniq_id,
+    'name' : name,
+    'age'  : age
+  }
+  origJson.users.push(usr);
+  origJson = JSON.stringify(origJson);
+  fs.writeFile('./storage.json', origJson, (err)=>{
+    if(err){
+      console.log(err);
+    }
+  });
+  res.sendStatus(200);
+})
+//Challenge 2
+app.get('/' , (req, res)=>{
+  var storage = fs.readFileSync('./storage.json','utf8',(err)=>{
+    console.log(storage);
+  });
+  res.json(storage);
+})
+//Challenge 3 + Modified for Challenge 4
+app.get('/:id',(req,res)=>{
+  let storage = fs.readFileSync('./storage.json','utf8',(err)=>{
+    console.log(storage);
+  });
+  let result = false ;
+  // for (let key in JSON.parse(storage)){
+  //   result = JSON.parse(storage).users.id == [req.params.id] ? true : false;
+  // }
+  var users = JSON.parse(storage).users ;
+  var user_found ;
+  users.forEach((user)=>{
+    if(user.id == [req.params.id]){
+      result = true
+      user_found = user;
+    }
+  })
 
-app.use(function(req, res) {
-  res.sendStatus(404);
-});
+  if(result){
+    res.send(`Found user_id : ${user_found.id} \nUser Name: ${user_found.name}`);
+  }else{
+    res.send("NOT FOUND");
+  }
 
+})
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
--- server.js
+++ server.js
@@ -3,16 +3,10 @@ let port = process.env.port || 8000;
 let app = express();
 let path = require('path');
 
-// app.use(function(req, res) {
-//   res.sendStatus(404);
-// });
-
 //Challenge 1
 app.get('/hello',(req,res)=>{
   res.send("Hello!");
 })
-
-
 //Challenge 2
 app.post('/create/:name',(req,res)=>{
   let response = {
@@ -26,18 +20,6 @@ app.post('/create/:name',(req,res)=>{
 app.get('/',(req,res)=>{
   res.sendFile(path.join(__dirname,'index.html'));
 })
-
-//Challenge 4
-app.get('/verify/:age',(req,res)=>{
-  let age = req.params.age;
-  if(age > 13){
-    res.sendStatus(200);
-  }else{
-    res.sendStatus(403);
-  }
-})
-
-
 
 
 
